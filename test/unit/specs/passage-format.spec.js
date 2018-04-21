c9a4aef9a27e8e3015d6a52af3be5a313e07269d
birdbird314 <grantstadt@gmail.com>
2018-04-21 16:47:13 +0200

    Replace dummy passages with real ones

--- test/unit/specs/passage-format.spec.js
+++ test/unit/specs/passage-format.spec.js
@@ -1,98 +1,110 @@
 import format from '@/passage-format';
 
 test('Should format single verse', () => {
-  const passage = {'Foo' : {'1' : {'2' : 'Bar, baz'}}};
+  const passage = {'Gen' : {'1' : {'3' : 'And God said, Let there be light: and there was light.'}}};
   const template = '${book} ${chapter}:${verse} "${text}"';
 
   const result = format(template, passage);
 
-  expect(result).toBe('Foo 1:2 "Bar, baz"');
+  expect(result).toBe('Gen 1:3 "And God said, Let there be light: and there was light."');
 });
 
 test('Should format multiple verses', () => {
-  const passage = {'Foo' :
-    {'1' :
-      {'2' : 'Bar, bar.',
-       '3' : 'Baz baz.',
-       '4' : 'Quix, quix.'}
+  const passage = {
+    'Gen': {
+      '1': {
+        '3': 'And God said, Let there be light: and there was light.',
+        '4': 'And God saw the light, that it was good: and God divided the light from the darkness.',
+      }
     }
   };
   const template = '${book} ${chapter}:${verseStart}-${verseEnd} "${text}"';
 
   const result = format(template, passage);
 
-  expect(result).toBe('Foo 1:2-4 "Bar, bar. Baz baz. Quix, quix."');
+  expect(result).toBe('Gen 1:3-4 "And God said, Let there be light: and there was light. And God saw the light, that it was good: and God divided the light from the darkness."');
 });
 
 test('Should format multiple verses with numbers', () => {
-  const passage = {'Foo' :
-    {'1' :
-      {'2' : 'Bar, bar.',
-       '3' : 'Baz baz.',
-       '4' : 'Quix, quix.'}
+  const passage = {
+    'Gen': {
+      '1': {
+        '3': 'And God said, Let there be light: and there was light.',
+        '4': 'And God saw the light, that it was good: and God divided the light from the darkness.',
+      }
     }
   };
   const template = '${book} ${chapter}:${verseStart}-${verseEnd} "${textWithNumbers}"';
 
   const result = format(template, passage);
 
-  expect(result).toBe('Foo 1:2-4 "(2) Bar, bar. (3) Baz baz. (4) Quix, quix."');
+  expect(result).toBe('Gen 1:3-4 "(3) And God said, Let there be light: and there was light. (4) And God saw the light, that it was good: and God divided the light from the darkness."');
 });
 
 test('Should format multiple verses with line breaks', () => {
-  const passage = {'Foo' :
-    {'1' :
-      {'2' : 'Bar, bar.',
-       '3' : 'Baz baz.',
-       '4' : 'Quix, quix.'}
+  const passage = {
+    'Gen': {
+      '1': {
+        '3': 'And God said, Let there be light: and there was light.',
+        '4': 'And God saw the light, that it was good: and God divided the light from the darkness.',
+      }
     }
   };
   const template = '${book} ${chapter}:${verseStart}-${verseEnd} ${textWithLineBreaks}';
 
   const result = format(template, passage);
 
-  expect(result).toBe('Foo 1:2-4 \nBar, bar.\nBaz baz.\nQuix, quix.');
+  expect(result).toBe('Gen 1:3-4 \nAnd God said, Let there be light: and there was light.\nAnd God saw the light, that it was good: and God divided the light from the darkness.');
 });
 
 test('Should format multiple verses with numbers and line breaks', () => {
-  const passage = {'Foo' :
-    {'1' :
-      {'2' : 'Bar, bar.',
-       '3' : 'Baz baz.',
-       '4' : 'Quix, quix.'}
+  const passage = {
+    'Gen': {
+      '1': {
+        '3': 'And God said, Let there be light: and there was light.',
+        '4': 'And God saw the light, that it was good: and God divided the light from the darkness.',
+      }
     }
   };
   const template = '${book} ${chapter}:${verseStart}-${verseEnd} ${textWithNumbersAndLineBreaks}';
 
   const result = format(template, passage);
 
-  expect(result).toBe('Foo 1:2-4 \n(2) Bar, bar.\n(3) Baz baz.\n(4) Quix, quix.');
+  expect(result).toBe('Gen 1:3-4 \n(3) And God said, Let there be light: and there was light.\n(4) And God saw the light, that it was good: and God divided the light from the darkness.');
 });
 
 test('Should format multiple chapters', () => {
   const passage = {
-    'Foo' : {
-      '1' : {'1' : 'Bar, bar.'},
-      '2' : {'1' : 'Baz, baz.' }
+    'Gen': {
+      '1': {
+        '31': 'And God saw every thing that he had made, and, behold, it was very good. And the evening and the morning were the sixth day.'
+      },
+      '2': {
+        '1': 'Thus the heavens and the earth were finished, and all the host of them.'
+      }
     }
   };
   const template = '${book} ${chapterStart}:${verseStart}-${chapterEnd}:${verseEnd} "${text}"';
 
   const result = format(template, passage);
 
-  expect(result).toBe('Foo 1:1-2:1 "Bar, bar. Baz, baz."');
+  expect(result).toBe('Gen 1:31-2:1 "And God saw every thing that he had made, and, behold, it was very good. And the evening and the morning were the sixth day. Thus the heavens and the earth were finished, and all the host of them."');
 });
 
 test('Should format multiple chapters with numbers', () => {
   const passage = {
-    'Foo' : {
-      '1' : {'1' : 'Bar, bar.'},
-      '2' : {'1' : 'Baz, baz.' }
+    'Gen': {
+      '1': {
+        '31': 'And God saw every thing that he had made, and, behold, it was very good. And the evening and the morning were the sixth day.'
+      },
+      '2': {
+        '1': 'Thus the heavens and the earth were finished, and all the host of them.'
+      }
     }
   };
   const template = '${book} ${chapterStart}:${verseStart}-${chapterEnd}:${verseEnd} "${textWithNumbers}"';
 
   const result = format(template, passage);
 
-  expect(result).toBe('Foo 1:1-2:1 "(1:1) Bar, bar. (2:1) Baz, baz."');
+  expect(result).toBe('Gen 1:31-2:1 "(1:31) And God saw every thing that he had made, and, behold, it was very good. And the evening and the morning were the sixth day. (2:1) Thus the heavens and the earth were finished, and all the host of them."');
 });
