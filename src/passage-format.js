0c39fc4987799106c55703d4e7269440d486931b
birdbird314 <grantstadt@gmail.com>
2018-04-25 17:38:26 +0200

    Refactor passage-format.js
    * Remove redundant semicolons
    * Move attributeFormatterMap to contentFormatters function

--- src/passage-format.js
+++ src/passage-format.js
@@ -1,35 +1,35 @@
 export default class Formatter {
   constructor() {
-    this.templates = {};
+    this.templates = {}
   }
 
   addTemplate({name: name, template: template}) {
-    this.templates[name] = template;
+    this.templates[name] = template
   }
 
   removeTemplate(name) {
-    delete this.templates[name];
+    delete this.templates[name]
   }
 
   format(templateName, passage) {
-    return format(this.templates[templateName], passage);
+    return format(this.templates[templateName], passage)
   }
 }
 
 function format(template, passage) {
-  const bookName = Object.keys(passage)[0];
-  const bookContent = passage[bookName];
+  const bookName = Object.keys(passage)[0]
+  const bookContent = passage[bookName]
 
-  const chapterNumbers = Object.keys(bookContent);
-  const chapterStart = chapterNumbers[0];
-  const chapterEnd = last(chapterNumbers);
+  const chapterNumbers = Object.keys(bookContent)
+  const chapterStart = chapterNumbers[0]
+  const chapterEnd = last(chapterNumbers)
 
-  const verseStart = Object.keys(bookContent[chapterStart])[0];
-  const verseEnd = last(Object.keys(bookContent[chapterEnd]));
+  const verseStart = Object.keys(bookContent[chapterStart])[0]
+  const verseEnd = last(Object.keys(bookContent[chapterEnd]))
 
-  const verses = versesFrom(bookContent);
+  const verses = versesFrom(bookContent)
 
-  const formatters = contentFormatters(template);
+  const formatters = contentFormatters(template)
 
   return formatWith({
     template: template,
@@ -50,62 +50,51 @@ function format(template, passage) {
 function versesFrom(bookContent) {
   return Object.entries(bookContent)
     .map(hasMoreThanOneChapter(bookContent) ? extractVersesWithChapterNumber : extractVerses)
-    .reduce(concat);
+    .reduce(concat)
 }
 
 function extractVersesWithChapterNumber([chapter, verses]) {
-  return Object.entries(verses).map(([verse, text]) => [`${chapter}:${verse}`, text]);
+  return Object.entries(verses).map(([verse, text]) => [`${chapter}:${verse}`, text])
 }
 
 function extractVerses([chapter, verses]) {
-  return Object.entries(verses);
+  return Object.entries(verses)
 }
 
 function concat(x, y) {
-  return x.concat(y);
+  return x.concat(y)
 }
 
 function last(arr) {
-  return arr[arr.length - 1];
+  return arr[arr.length - 1]
 }
 
 function hasMoreThanOneChapter(bookContent) {
-  return Object.keys(bookContent).length > 1;
-}
-
-const attributeFormatterMap = {
-  textWithNumbersAndLineBreaks: verses => '\n' + verses.map(([k, v]) => `(${k}) ${v}`).join('\n'),
-  textWithNumbers: verses => verses.map(([k, v]) => `(${k}) ${v}`).join(' '),
-  textWithLineBreaks: verses => '\n' + verses.map(([k, v]) => v).join('\n'),
-  text: verses => verses.map(([k, v]) => v).join(' ')
+  return Object.keys(bookContent).length > 1
 }
 
 function contentFormatters(template) {
-  const formatters = objectWithDefaultValue(() => undefined);
-  const [attr, formatter] = Object.entries(attributeFormatterMap).find(([k, v]) => template.includes(k));
-  formatters[attr] = formatter;
-  return formatters;
+  const attributeFormatterMap = {
+    textWithNumbersAndLineBreaks: verses => '\n' + verses.map(([k, v]) => `(${k}) ${v}`).join('\n'),
+    textWithNumbers: verses => verses.map(([k, v]) => `(${k}) ${v}`).join(' '),
+    textWithLineBreaks: verses => '\n' + verses.map(([k, v]) => v).join('\n'),
+    text: verses => verses.map(([k, v]) => v).join(' ')
+  }
+  const formatters = objectWithDefaultValue(() => undefined)
+  const [attr, formatter] = Object.entries(attributeFormatterMap).find(([k, v]) => template.includes(k))
+  formatters[attr] = formatter
+  return formatters
 }
 
 function objectWithDefaultValue(defaultValue) {
   return new Proxy({}, {
     get: (target, name) => name in target ? target[name] : defaultValue
-  });
+  })
 }
 
 function formatWith({
-  template: template,
-  book: book,
-  chapter: chapter,
-  chapterStart: chapterStart,
-  chapterEnd: chapterEnd,
-  verse: verse,
-  verseStart: verseStart,
-  verseEnd: verseEnd,
-  text: text,
-  textWithNumbers: textWithNumbers,
-  textWithLineBreaks: textWithLineBreaks,
-  textWithNumbersAndLineBreaks: textWithNumbersAndLineBreaks
+  template, book, chapter, chapterStart, chapterEnd, verse, verseStart,
+  verseEnd, text, textWithNumbers, textWithLineBreaks, textWithNumbersAndLineBreaks,
 }) {
-  return eval('`' + template + '`');
+  return eval('`' + template + '`')
 }
--- test/unit/specs/passage-format.spec.js
+++ test/unit/specs/passage-format.spec.js
@@ -1,9 +1,9 @@
-import Formatter from '@/passage-format';
+import Formatter from '@/passage-format'
 
-let formatter;
+let formatter
 
 beforeEach(() => {
-  formatter = new Formatter();
+  formatter = new Formatter()
 })
 
 test('Should add new template', () => {
@@ -12,8 +12,8 @@ test('Should add new template', () => {
     template: 'some template'
   })
 
-  expect(formatter.templates['some name']).toBe('some template');
-});
+  expect(formatter.templates['some name']).toBe('some template')
+})
 
 test('Should remove template', () => {
   formatter.addTemplate({
@@ -21,22 +21,22 @@ test('Should remove template', () => {
     template: 'some template'
   })
 
-  formatter.removeTemplate('some name');
+  formatter.removeTemplate('some name')
 
-  expect(formatter.templates['some name']).toBeUndefined();
-});
+  expect(formatter.templates['some name']).toBeUndefined()
+})
 
 test('Should format single verse', () => {
-  const passage = {'Gen' : {'1' : {'3' : 'And God said, Let there be light: and there was light.'}}};
+  const passage = {'Gen' : {'1' : {'3' : 'And God said, Let there be light: and there was light.'}}}
   formatter.addTemplate({
     name: 'single verse',
     template: '${book} ${chapter}:${verse} "${text}"'
   })
 
-  const result = formatter.format('single verse', passage);
+  const result = formatter.format('single verse', passage)
 
-  expect(result).toBe('Gen 1:3 "And God said, Let there be light: and there was light."');
-});
+  expect(result).toBe('Gen 1:3 "And God said, Let there be light: and there was light."')
+})
 
 test('Should format multiple verses', () => {
   const passage = {
@@ -46,16 +46,16 @@ test('Should format multiple verses', () => {
         '4': 'And God saw the light, that it was good: and God divided the light from the darkness.',
       }
     }
-  };
+  }
   formatter.addTemplate({
     name: 'multiple verses',
     template: '${book} ${chapter}:${verseStart}-${verseEnd} "${text}"'
   })
 
-  const result = formatter.format('multiple verses', passage);
+  const result = formatter.format('multiple verses', passage)
 
-  expect(result).toBe('Gen 1:3-4 "And God said, Let there be light: and there was light. And God saw the light, that it was good: and God divided the light from the darkness."');
-});
+  expect(result).toBe('Gen 1:3-4 "And God said, Let there be light: and there was light. And God saw the light, that it was good: and God divided the light from the darkness."')
+})
 
 test('Should format multiple verses with numbers', () => {
   const passage = {
@@ -65,16 +65,16 @@ test('Should format multiple verses with numbers', () => {
         '4': 'And God saw the light, that it was good: and God divided the light from the darkness.',
       }
     }
-  };
+  }
   formatter.addTemplate({
     name: 'verses with numbers',
     template: '${book} ${chapter}:${verseStart}-${verseEnd} "${textWithNumbers}"'
   })
 
-  const result = formatter.format('verses with numbers', passage);
+  const result = formatter.format('verses with numbers', passage)
 
-  expect(result).toBe('Gen 1:3-4 "(3) And God said, Let there be light: and there was light. (4) And God saw the light, that it was good: and God divided the light from the darkness."');
-});
+  expect(result).toBe('Gen 1:3-4 "(3) And God said, Let there be light: and there was light. (4) And God saw the light, that it was good: and God divided the light from the darkness."')
+})
 
 test('Should format multiple verses with line breaks', () => {
   const passage = {
@@ -84,16 +84,16 @@ test('Should format multiple verses with line breaks', () => {
         '4': 'And God saw the light, that it was good: and God divided the light from the darkness.',
       }
     }
-  };
+  }
   formatter.addTemplate({
     name: 'verses with line breaks',
     template: '${book} ${chapter}:${verseStart}-${verseEnd} ${textWithLineBreaks}'
   })
 
-  const result = formatter.format('verses with line breaks', passage);
+  const result = formatter.format('verses with line breaks', passage)
 
-  expect(result).toBe('Gen 1:3-4 \nAnd God said, Let there be light: and there was light.\nAnd God saw the light, that it was good: and God divided the light from the darkness.');
-});
+  expect(result).toBe('Gen 1:3-4 \nAnd God said, Let there be light: and there was light.\nAnd God saw the light, that it was good: and God divided the light from the darkness.')
+})
 
 test('Should format multiple verses with numbers and line breaks', () => {
   const passage = {
@@ -103,16 +103,16 @@ test('Should format multiple verses with numbers and line breaks', () => {
         '4': 'And God saw the light, that it was good: and God divided the light from the darkness.',
       }
     }
-  };
+  }
   formatter.addTemplate({
     name: 'verses with numbers and line breaks',
     template: '${book} ${chapter}:${verseStart}-${verseEnd} ${textWithNumbersAndLineBreaks}'
   })
 
-  const result = formatter.format('verses with numbers and line breaks', passage);
+  const result = formatter.format('verses with numbers and line breaks', passage)
 
-  expect(result).toBe('Gen 1:3-4 \n(3) And God said, Let there be light: and there was light.\n(4) And God saw the light, that it was good: and God divided the light from the darkness.');
-});
+  expect(result).toBe('Gen 1:3-4 \n(3) And God said, Let there be light: and there was light.\n(4) And God saw the light, that it was good: and God divided the light from the darkness.')
+})
 
 test('Should format multiple chapters', () => {
   const passage = {
@@ -124,16 +124,16 @@ test('Should format multiple chapters', () => {
         '1': 'Thus the heavens and the earth were finished, and all the host of them.'
       }
     }
-  };
+  }
   formatter.addTemplate({
     name: 'cross-chapters passage',
     template: '${book} ${chapterStart}:${verseStart}-${chapterEnd}:${verseEnd} "${text}"'
   })
 
-  const result = formatter.format('cross-chapters passage', passage);
+  const result = formatter.format('cross-chapters passage', passage)
 
-  expect(result).toBe('Gen 1:31-2:1 "And God saw every thing that he had made, and, behold, it was very good. And the evening and the morning were the sixth day. Thus the heavens and the earth were finished, and all the host of them."');
-});
+  expect(result).toBe('Gen 1:31-2:1 "And God saw every thing that he had made, and, behold, it was very good. And the evening and the morning were the sixth day. Thus the heavens and the earth were finished, and all the host of them."')
+})
 
 test('Should format multiple chapters with numbers', () => {
   const passage = {
@@ -145,13 +145,13 @@ test('Should format multiple chapters with numbers', () => {
         '1': 'Thus the heavens and the earth were finished, and all the host of them.'
       }
     }
-  };
+  }
   formatter.addTemplate({
     name: 'cross-chapters passage with numbers',
     template: '${book} ${chapterStart}:${verseStart}-${chapterEnd}:${verseEnd} "${textWithNumbers}"'
   })
 
-  const result = formatter.format('cross-chapters passage with numbers', passage);
+  const result = formatter.format('cross-chapters passage with numbers', passage)
 
-  expect(result).toBe('Gen 1:31-2:1 "(1:31) And God saw every thing that he had made, and, behold, it was very good. And the evening and the morning were the sixth day. (2:1) Thus the heavens and the earth were finished, and all the host of them."');
-});
+  expect(result).toBe('Gen 1:31-2:1 "(1:31) And God saw every thing that he had made, and, behold, it was very good. And the evening and the morning were the sixth day. (2:1) Thus the heavens and the earth were finished, and all the host of them."')
+})
