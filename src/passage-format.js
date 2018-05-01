5bd1c2056cb9c7a760fc52982e2fed017f014ee4
birdbird314 <grantstadt@gmail.com>
2018-05-01 13:33:56 +0200

    Format verses according to given template (#2)
    Format verses according to given template

---
+++ src/passage-format.js
@@ -0,0 +1,91 @@
+export default class Formatter {
+  constructor() {
+    this.templates = {}
+  }
+
+  addTemplate({name: name, template: template}) {
+    this.templates[name] = template
+  }
+
+  removeTemplate(name) {
+    delete this.templates[name]
+  }
+
+  setCurrentTemplate(name) {
+    this.currentTemplate = this.templates[name]
+    this.formatVersesFn = formatVersesFnFor(this.currentTemplate)
+  }
+
+  format(passages) {
+    return format(this.currentTemplate, this.formatVersesFn, passages)
+  }
+}
+
+function format(template, formatVersesFn, passages) {
+  return passages
+    .map(singleBookPassage => formatSingleBook(template, formatVersesFn, singleBookPassage))
+    .join('\n\n')
+}
+
+function formatSingleBook(template, formatVersesFn, passage) {
+  const book = Object.keys(passage)[0]
+  const chapters = Object.entries(passage[book])
+
+  let chapter, chapterStart;
+  chapter = chapterStart = chapters[0][0]
+  const chapterEnd = last(chapters)[0]
+
+  let verse, verseStart
+  verse = verseStart = firstVerseNumber(chapters)
+  const verseEnd = lastVerseNumber(chapters)
+
+  let text, textWithNumbers, textWithLineBreaks, textWithNumbersAndLineBreaks
+  text = textWithNumbers = textWithLineBreaks = textWithNumbersAndLineBreaks = formatVersesFn(versesFrom(chapters))
+
+  /*
+    eval expression below expects the following variables to be defined:
+      book, chapter, chapterStart, chapterEnd, verse, verseStart, verseEnd,
+      text, textWithNumbers, textWithLineBreaks, textWithNumbersAndLineBreaks
+  */
+  return eval('`' + template + '`')
+}
+
+function firstVerseNumber(chapters) {
+  return Object.keys(chapters[0][1])[0];
+}
+
+function lastVerseNumber(chapters) {
+  return last(Object.keys(last(chapters)[1]))
+}
+
+function versesFrom(chapters) {
+  return chapters
+    .map(chapters.length > 1 ? extractVersesWithChapterNumber : extractVerses)
+    .reduce(concat)
+}
+
+function extractVersesWithChapterNumber([chapter, verses]) {
+  return Object.entries(verses).map(([verse, text]) => [`${chapter}:${verse}`, text])
+}
+
+function extractVerses([chapter, verses]) {
+  return Object.entries(verses)
+}
+
+function formatVersesFnFor(template) {
+  const [_, fn] = Object.entries({
+    textWithNumbersAndLineBreaks: verses => '\n' + verses.map(([k, v]) => `(${k}) ${v}`).join('\n'),
+    textWithNumbers: verses => verses.map(([k, v]) => `(${k}) ${v}`).join(' '),
+    textWithLineBreaks: verses => '\n' + verses.map(([k, v]) => v).join('\n'),
+    text: verses => verses.map(([k, v]) => v).join(' ')
+  }).find(([k, v]) => template.includes(k))
+  return fn;
+}
+
+function concat(x, y) {
+  return x.concat(y)
+}
+
+function last(arr) {
+  return arr[arr.length - 1]
+}
---
+++ test/unit/specs/passage-format.spec.js
@@ -0,0 +1,165 @@
+import Formatter from '@/passage-format'
+
+let formatter
+
+beforeEach(() => {
+  formatter = new Formatter()
+})
+
+test('Should add new template', () => {
+  formatter.addTemplate({
+    name: 'some name',
+    template: 'some template'
+  })
+
+  expect(formatter.templates['some name']).toBe('some template')
+})
+
+test('Should remove template', () => {
+  formatter.addTemplate({
+    name: 'some name',
+    template: 'some template'
+  })
+
+  formatter.removeTemplate('some name')
+
+  expect(formatter.templates['some name']).toBeUndefined()
+})
+
+test('Should format single verse', () => {
+  verifyFormat({
+    passages: [{'Gen' : {'1' : {'3' : 'And God said, Let there be light: and there was light.'}}}],
+    template: '${book} ${chapter}:${verse} "${text}"',
+
+    expected: 'Gen 1:3 "And God said, Let there be light: and there was light."'
+  })
+})
+
+test('Should format multiple verses', () => {
+  verifyFormat({
+    passages: [{
+      'Gen': {
+        '1': {
+          '3': 'And God said, Let there be light: and there was light.',
+          '4': 'And God saw the light, that it was good: and God divided the light from the darkness.',
+        }
+      }
+    }],
+    template: '${book} ${chapter}:${verseStart}-${verseEnd} "${text}"',
+
+    expected: 'Gen 1:3-4 "And God said, Let there be light: and there was light. And God saw the light, that it was good: and God divided the light from the darkness."'
+  })
+})
+
+test('Should format multiple verses with numbers', () => {
+  verifyFormat({
+    passages: [{
+      'Gen': {
+        '1': {
+          '3': 'And God said, Let there be light: and there was light.',
+          '4': 'And God saw the light, that it was good: and God divided the light from the darkness.',
+        }
+      }
+    }],
+    template: '${book} ${chapter}:${verseStart}-${verseEnd} "${textWithNumbers}"',
+
+    expected: 'Gen 1:3-4 "(3) And God said, Let there be light: and there was light. (4) And God saw the light, that it was good: and God divided the light from the darkness."'
+  })
+})
+
+test('Should format multiple verses with line breaks', () => {
+  verifyFormat({
+    passages: [{
+      'Gen': {
+        '1': {
+          '3': 'And God said, Let there be light: and there was light.',
+          '4': 'And God saw the light, that it was good: and God divided the light from the darkness.',
+        }
+      }
+    }],
+    template: '${book} ${chapter}:${verseStart}-${verseEnd} ${textWithLineBreaks}',
+    expected: 'Gen 1:3-4 \nAnd God said, Let there be light: and there was light.\nAnd God saw the light, that it was good: and God divided the light from the darkness.'
+  })
+})
+
+test('Should format multiple verses with numbers and line breaks', () => {
+  verifyFormat({
+    passages: [{
+      'Gen': {
+        '1': {
+          '3': 'And God said, Let there be light: and there was light.',
+          '4': 'And God saw the light, that it was good: and God divided the light from the darkness.',
+        }
+      }
+    }],
+    template: '${book} ${chapter}:${verseStart}-${verseEnd} ${textWithNumbersAndLineBreaks}',
+    expected: 'Gen 1:3-4 \n(3) And God said, Let there be light: and there was light.\n(4) And God saw the light, that it was good: and God divided the light from the darkness.'
+  })
+})
+
+test('Should format multiple chapters', () => {
+  verifyFormat({
+    passages: [{
+      'Gen': {
+        '1': {
+          '31': 'And God saw every thing that he had made, and, behold, it was very good. And the evening and the morning were the sixth day.'
+        },
+        '2': {
+          '1': 'Thus the heavens and the earth were finished, and all the host of them.'
+        }
+      }
+    }],
+    template: '${book} ${chapterStart}:${verseStart}-${chapterEnd}:${verseEnd} "${text}"',
+    expected: 'Gen 1:31-2:1 "And God saw every thing that he had made, and, behold, it was very good. And the evening and the morning were the sixth day. Thus the heavens and the earth were finished, and all the host of them."'
+  })
+})
+
+test('Should format multiple chapters with numbers', () => {
+  verifyFormat({
+    passages: [{
+      'Gen': {
+        '1': {
+          '31': 'And God saw every thing that he had made, and, behold, it was very good. And the evening and the morning were the sixth day.'
+        },
+        '2': {
+          '1': 'Thus the heavens and the earth were finished, and all the host of them.'
+        }
+      }
+    }],
+    template: '${book} ${chapterStart}:${verseStart}-${chapterEnd}:${verseEnd} "${textWithNumbers}"',
+    expected: 'Gen 1:31-2:1 "(1:31) And God saw every thing that he had made, and, behold, it was very good. And the evening and the morning were the sixth day. (2:1) Thus the heavens and the earth were finished, and all the host of them."'
+  })
+})
+
+test('Should format multiple passages', () => {
+  verifyFormat({
+    passages: [
+      {'Gen' : {'1' : {'3' : 'And God said, Let there be light: and there was light.'}}},
+      {'Ecc' : {'1' : {'2' : 'Vanity of vanities, saith the Preacher, vanity of vanities; all is vanity.'}}}
+    ],
+    template: '${book} ${chapter}:${verse} "${text}"',
+
+    expected: 'Gen 1:3 "And God said, Let there be light: and there was light."\n\nEcc 1:2 "Vanity of vanities, saith the Preacher, vanity of vanities; all is vanity."'
+  })
+})
+
+test('Should not add any additional linebreaks between passages when ${textWithLineBreaks} is used in tempalte', () => {
+  verifyFormat({
+    passages: [
+      {'Gen' : {'1' : {'3' : 'And God said, Let there be light: and there was light.'}}},
+      {'Ecc' : {'1' : {'2' : 'Vanity of vanities, saith the Preacher, vanity of vanities; all is vanity.'}}}
+    ],
+    template: '${book} ${chapter}:${verse} ${textWithLineBreaks}',
+
+    expected: 'Gen 1:3 \nAnd God said, Let there be light: and there was light.\n\nEcc 1:2 \nVanity of vanities, saith the Preacher, vanity of vanities; all is vanity.'
+  })
+})
+
+function verifyFormat({passages, template, expected}) {
+  formatter.addTemplate({
+    name: 'some name',
+    template: template
+  })
+  formatter.setCurrentTemplate('some name')
+  expect(formatter.format(passages)).toBe(expected)
+}
