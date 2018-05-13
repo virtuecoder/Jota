1f896b7c1ea61f4d3d50577e0f8a1fd711ad6a64
Jacek Kolodziejczyk <jacek.p.kolodziejczyk@gmail.com>
2018-05-13 21:38:04 +0200

    Reader page scafold, enter passage reference as: gen[1][1]

---
+++ src/components/Reader.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>Bible Reader</h1>
+
+    <div class="ui grid container">
+      <div class="ui input eight wide column">
+        <input type="text" v-model="refInput" placeholder="Input passage reference..." style="width: 100%" @input="display"/>
+      </div>
+      <div class="ui input eight wide column">
+        <input type="text" v-model="search" placeholder="Search..." style="width: 100%" />
+      </div>
+
+      <div>
+        {{ text }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+/* eslint no-unused-vars: "off" */
+import bible from '../../data/pl/bible/ubg.json'
+
+export default {
+  data () {
+    return {
+      refInput: '',
+      search: '',
+      text: '',
+      bible
+    }
+  },
+  methods: {
+    display () {
+      /* eslint no-eval: "off" */
+      this.text = eval('this.bible.' + this.refInput)
+    }
+  }
+}
+</script>
